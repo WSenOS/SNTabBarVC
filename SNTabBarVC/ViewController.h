93fea9cb3f54d0280470eabe632092a3b298fc11
w_sen <wangsenwd@hotmail.com>
2016-03-26 10:54:07 +0800

    Initial Commit

---
+++ SNTabBarVC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SNTabBarVC
+//
+//  Created by wangsen on 16/3/26.
+//  Copyright © 2016年 wangsen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SNTabBarVC/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  SNTabBarVC
+//
+//  Created by wangsen on 16/3/26.
+//  Copyright © 2016年 wangsen. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
